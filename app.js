0db138aded7ddc34c0ecef266c45f491b2733a98
Christian Simpson <ChristianRSimpson25@gmail.com>
2015-10-12 21:20:46 -0400

    Change the routes

--- app.js
+++ app.js
@@ -26,7 +26,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/', projects);
+app.use('/projects', projects);
 app.use('/', about);
 app.use('/', contact);
 app.use('/', services);
